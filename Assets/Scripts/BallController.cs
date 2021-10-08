ae8b69c0cae786101e17cdc048722b12a0f84014
Rizky Alfiansyah <Ralfiansyah111@gmail.com>
2021-10-08 17:01:36 +0700

    Chapter 10 Problem Solving 2

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Rigidbody 2D dari bola
+    private Rigidbody2D rigidBody2D;
+
+    // Perintah untuk Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        // Mulai game
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PushBall()
+    {
+        // nilai komponen y dari -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    // perintah untuk mereset bola
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+   // Perintah untuk merestart Game
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+}
