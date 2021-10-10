b61facc1272ed3825740c7ef708b487043bc712c
Rizky Alfiansyah <Ralfiansyah111@gmail.com>
2021-10-10 17:36:10 +0700

    Chapter 10 Problem 9

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonController : MonoBehaviour
+{
+    
+    public void scale(float scale)
+    {
+        transform.localScale = new Vector2(1 / scale, 1 * scale);
+    }
+
+    // fungsi pemanggilan scene 
+
+    public void Scene(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+	public Transform[] Positions;
+	public GameObject Object;
+
+	public Transform Location;
+
+	public bool ToSpawn = true;
+
+	// Start is called before the first frame update
+	void Update()
+	{
+		// perintah untuk mengatur posisi kotak secara random dimunculkan di tiap area
+		Location = Positions[Random.Range(0, Positions.Length)];
+
+		if (ToSpawn == true)
+		{
+			Instantiate(Object, Location);
+			ToSpawn = false;
+			StartCoroutine(ToSpawnTrue());
+		}
+	}
+
+	// Perintah untuk mengatur waktu muncul nya kotak selama beberapa detik
+
+	IEnumerator ToSpawnTrue()
+	{
+		yield return new WaitForSeconds(3f);
+		ToSpawn = true;
+	}
+}
